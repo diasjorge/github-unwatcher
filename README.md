c3053608d4881cfe13bb14ce50d53529bc805545
Jorge Dias <jorge.dias@schibsted.com>
2016-04-06 15:57:41 +0200

    Unwatcher first draft

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # github-unwatcher
+
+Tired of receiving notifications from too many repositories?
+
+github-unwatcher can help. It will ignore notifications from all repositories you don't own and have not contributed.
+
+## Installation
+
+bundle install
+
+## Usage
+
+```
+[OCTOKIT_API_ENDPOINT=https://YOUR_GITHUB_ENTERPRISE/api/v3] OCTOKIT_ACCESS_TOKEN=YOUR_PERSONAL_ACCESS_TOKEN UNWATCHER_LOGIN=YOUR_LOGIN bundle exec ruby unwatcher.rb
+```
---
+++ unwatcher.rb
@@ -0,0 +1,50 @@
+require 'octokit'
+
+# OCTOKIT_API_ENDPOINT
+# OCTOKIT_ACCESS_TOKEN
+# UNWATCHER_LOGIN
+
+def client
+  @client ||= Octokit.client
+end
+
+def owner?(repo, login)
+  repo[:owner][:login] == login
+end
+
+def contributor?(repo, login)
+  contributors = client.contributors(repo.full_name)
+
+  # When repo is empty list of contributors is an empty string, so user can not be contributor
+  return false if contributors == ""
+
+  contributors.any? { |contributor| contributor[:login]  == login }
+end
+
+login = ENV.fetch('UNWATCHER_LOGIN')
+
+# stack = Faraday::RackBuilder.new do |builder|
+#   builder.response :logger
+#   builder.use Octokit::Response::RaiseError
+#   builder.adapter Faraday.default_adapter
+# end
+
+# Octokit.middleware = stack
+
+Octokit.auto_paginate = true
+
+repos = client.repos
+
+repos.each do |repo|
+  if owner?(repo, login)
+    puts "Owner for #{repo.full_name}"
+    next
+  end
+
+  if contributor?(repo, login)
+    puts "Contributor for #{repo.full_name}"
+  else
+    puts "Not a contributor for #{repo.full_name}. Ignoring notifications"
+    client.update_subscription(repo.full_name, ignored: true)
+  end
+end
